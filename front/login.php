d36c958146f7c3448b07dc7c3067938a1b8b0361
hcyeeeee <auu0812@gmail.com>
2021-12-14 00:15:06 +0800

    新增檔案和資料夾

---
+++ front/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+<h2 class='text-center font-weight-bold'>會員登入</h2>
+
+<form action="./api/check_login.php" method="post">
+<table id="loginForm" class='table m-auto w-auto'>
+    <tr>
+        <td>帳號：</td>
+        <td><input type="text" name="account"></td>
+    </tr>
+    <tr>
+        <td>密碼</td>
+        <td><input type="password" name="password"></td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            <input type="submit" value="登入">
+            <input type="reset" value="重罝">
+        </td>
+
+    </tr>
+</table>
+
+
+</form>
+</body>
+</html>
